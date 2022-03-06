71bda4b04ae260374f47a10eb85f6193e511a36f
Alberto Arroyo Raygada <beyondnet.peru@gmail.com>
2022-03-05 21:48:09 -0500

    Refactoring

--- src/Services/Ideas/MusicStore.Ideas.Application/Queries/IIdeaQueries.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MusicStore.Ideas.Application.Queries
-{
-    public interface IIdeaQueries
-    {
-        Task<Idea> GetIdeaAsync(string id);
-
-        Task<IEnumerable<IdeaSummary>> GetIdeasFromOwner(string ownerId);
-    }
-}

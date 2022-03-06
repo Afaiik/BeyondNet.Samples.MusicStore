b9e8a8f1d790d501bec476f85551ea636a786016
Alberto Arroyo Raygada <beyondnet.peru@gmail.com>
2022-03-05 23:02:14 -0500

    Refactoring

--- src/Services/Catalog/MusicStore.Catalog.Application/Commands/CreateIdeaCommand.cs
+++ src/Services/Catalog/MusicStore.Catalog.Application/Commands/CreateIdeaCommand.cs
@@ -1,41 +1,6 @@
 ﻿namespace MusicStore.Catalog.Application.Commands
 {
-    [DataContract]
-    public class CreateIdeaCommand : IRequest<bool>
-    {
-        [DataMember]
-        public string Name { get; }
-
-        [DataMember]
-        public string Description { get; }
-
-        [DataMember]
-        private readonly List<string> _tags;
-
-        [DataMember]
-        public IEnumerable<string> Tags => _tags.AsReadOnly();
-
-        [DataMember]
-        private readonly List<Resource> _resources;
-
-        [DataMember]
-        public IEnumerable<Resource> Resources => _resources.AsReadOnly();
-
-
-        private CreateIdeaCommand()
-        {
-            _resources = new List<Resource>();
-        }
-
-        public CreateIdeaCommand(string name, string description, List<string> tags, List<Resource> resources) : this()
-        {
-            Name = name;
-            Description = description;
-            _tags = tags;
-            _resources = resources;
-        }
-
-    }
+    public record CreateIdeaCommand(string name, string description, List<string> tags, List<Resource> resources) : IRequest<bool>;
 
     public record Resource
     {

6e49bf28f154e35ae151f8a970e5263c95c1a18a
Alberto Arroyo Raygada <beyondnet.peru@gmail.com>
2022-03-05 23:05:20 -0500

    Refactoring

--- src/Services/Catalog/MusicStore.Catalog.Application/Commands/CreateIdeaCommand.cs
+++ src/Services/Catalog/MusicStore.Catalog.Application/Commands/CreateIdeaCommand.cs
@@ -1,15 +1,5 @@
 ﻿namespace MusicStore.Catalog.Application.Commands
 {
-    public record CreateIdeaCommand(string name, string description, List<string> tags, List<Resource> resources) : IRequest<bool>;
-
-    public record Resource
-    {
-        public string Name { get; init; }
-
-        public string Path { get; init; }
-
-        public bool IsExternal { get; init; }
-
-        public bool IsShared { get; init; }
-    }
+    public record CreateIdeaCommand(string Name, string Description, List<string> Tags, List<Resource> Resources) : IRequest<bool>;
+    public record Resource(string Name, string Path, bool IsExternal, bool IsShared);
 }
